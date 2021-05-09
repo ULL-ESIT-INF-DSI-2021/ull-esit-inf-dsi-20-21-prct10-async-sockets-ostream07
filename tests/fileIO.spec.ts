c6807959b7b3ec578a74ae4d10063458e9bb703f
Saúl García <alu0101129785@ull.edu.es>
2021-05-09 19:01:40 +0100

    Pruebas fileIO

--- tests/fileIO.spec.ts
+++ tests/fileIO.spec.ts
@@ -1,13 +1,20 @@
 import "mocha";
 import { expect } from "chai";
-import { saveNote, loadNotes } from "../src/fileIO";
+import { saveNote, loadNotes, removeNote } from "../src/fileIO";
 import { Color, Note } from "../src/note";
 
-describe("fileIO function tests", () => {
-    it("it saves a note and loads it back", () => {
-      const note1 = new Note("note1", Color.YELLOW, "yellow test");
-      const testUser = "testing";
-      saveNote(testUser, note1);
-      expect(loadNotes(testUser)).to.be.deep.equal([note1]);
-    });
+describe("fileIO functions tests", () => {
+  it("it saves a note and loads it back", () => {
+    const note1 = new Note("note1", Color.YELLOW, "yellow test");
+    const testUser = "testing";
+    saveNote(testUser, note1);
+    expect(loadNotes(testUser)).to.be.deep.equal([note1]);
+  });
+  it("it deletes a note and check that it is removed", () => {
+    const note1 = new Note("note1", Color.YELLOW, "yellow test");
+    const testUser = "testing";
+    saveNote(testUser, note1);
+    removeNote(testUser, note1.getTitle());
+    expect(loadNotes(testUser)).not.contains(note1);
+  });
 });
