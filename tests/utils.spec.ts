3f373f54753868f931987ef91d371616e7616361
Saúl García <alu0101129785@ull.edu.es>
2021-05-09 19:08:30 +0100

    Pruebas utils

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,7 +1,8 @@
 import "mocha";
 import { expect } from "chai";
-import { getNoteByTitle } from "../src/utils";
+import { getNoteByTitle, searchEntryIndex } from "../src/utils";
 import { Color, Note } from "../src/note";
+import { IndexEntry, NoteIndex } from "../src/interfaces";
 
 describe("utils function tests", () => {
   it("it search the title of a vector with some notes", () => {
@@ -17,5 +18,12 @@ describe("utils function tests", () => {
     const note3 = new Note("note3", Color.RED, "red test");
     const vectorTest = [note1, note2, note3];
     expect(getNoteByTitle("note4", vectorTest)).to.be.false;
+  });
+  it("it search an entry for a user index", () => {
+    const indexTest: NoteIndex = {"index":[{"title":"Yellow note","fileName":"Yellow_note.json"},
+    {"title":"Red note","fileName":"Red_note.json"},{"title":"Blue note","fileName":"Blue_note.json"},
+    {"title":"Green note","fileName":"Green_note.json"},{"title":"Black note","fileName":"Black_note.json"}]};
+    const entry: IndexEntry = {"title":"Blue note","fileName":"Blue_note.json"}; 
+    expect(searchEntryIndex(entry.title, indexTest)).to.be.deep.equal(entry);
   });
 });
