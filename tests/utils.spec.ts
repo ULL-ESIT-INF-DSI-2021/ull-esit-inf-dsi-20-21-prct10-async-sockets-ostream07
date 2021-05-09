14fccbd5e383822e176615f0fad5c9964b33f0ad
Saúl García <alu0101129785@ull.edu.es>
2021-05-09 19:16:51 +0100

    Pruebas utils

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,6 +1,6 @@
 import "mocha";
 import { expect } from "chai";
-import { getNoteByTitle, searchEntryIndex } from "../src/utils";
+import { getNoteByTitle, searchEntryIndex, getColorByString } from "../src/utils";
 import { Color, Note } from "../src/note";
 import { IndexEntry, NoteIndex } from "../src/interfaces";
 
@@ -19,6 +19,7 @@ describe("utils function tests", () => {
     const vectorTest = [note1, note2, note3];
     expect(getNoteByTitle("note4", vectorTest)).to.be.false;
   });
+
   it("it search an entry for a user index", () => {
     const indexTest: NoteIndex = {"index":[{"title":"Yellow note","fileName":"Yellow_note.json"},
     {"title":"Red note","fileName":"Red_note.json"},{"title":"Blue note","fileName":"Blue_note.json"},
@@ -32,5 +33,12 @@ describe("utils function tests", () => {
     {"title":"Green note","fileName":"Green_note.json"},{"title":"Black note","fileName":"Black_note.json"}]};
     const entry: IndexEntry = {"title":"Orange note","fileName":"Blue_note.json"}; 
     expect(searchEntryIndex(entry.title, indexTest)).to.be.false;
+  });
+
+  it("color", () => {
+    expect(getColorByString('rEd')).to.be.equal(Color.RED);
+  });
+  it("Checks if the color of the note is blue", () => {
+    expect(getColorByString('bLuE')).to.be.equal(Color.BLUE);
   });
 });
